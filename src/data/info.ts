5f3f3322498b38d489528bfe81a068ea65541755
Thanyapisit Buaprakong <nyman3527@gmail.com>
2023-06-10 18:54:55 +0700

    asd

---
+++ src/data/info.ts
@@ -0,0 +1,122 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Thanyapisit Buaprakhong",
+  jobDescription: "Web Developer",
+  about: `A dynamic 20-year-old Computer Science student with a profound interest in Artificial Intelligence and web programming. Currently working as a Software Engineer at CMSK, where I harness my technical abilities to drive innovation and efficiency. Passionate about harnessing the power of technology to create effective solutions, with a strong foundation in various programming languages and AI models. Known for my problem-solving skills and ability to think outside the box, I strive to improve and expand my knowledge in this ever-evolving field.
+  `,
+
+  experience: [
+    {
+      name: "Fulltime Artificial Intelligence Engineer, CMSK",
+      location: "Bangkok, Thailand", 
+      startDate: "Apr 2023",
+      endDate: "Present",
+      description: [
+        "- Chatbot Fine-tuning: I actively engage in fine-tuning the chatbot's natural language understanding (NLU) capabilities, aiming to improve its accuracy and efficiency in processing user queries This involves analyzing chatbot conversations, identifying patterns, and implementing necessary adjustments to optimize its responses.",
+        "- Quality Assurance: I meticulously test the chatbot's performance across various scenarios and user interactions to ensure its reliability and consistency. This includes conducting systematic testing, detecting and resolving errors or inconsistencies, and documenting issues for further development and improvement.",
+        "- Collaboration: I work closely with cross-functional teams, including developers, designers, and product managers, to understand business requirements and align chatbot functionalities with organizational goals.",
+        "- Research: I conduct research on the latest trends and developments in the field of AI and chatbot technology to identify opportunities for improvement and innovation.",
+      ],
+    },
+
+    {
+      name: "Apprentice Artificial Intelligence Engineer, Super AI Engineer Season3", 
+      location: "Bangkok, Thailand", 
+      startDate: "Nov 2022",
+      endDate: "Present",
+      description: [
+        "Advanced AI training course (Applied Level) attending workshops (Online & Onsite) Planning, design, analysis, system building and soft skills, group and individual work.to enhance knowledge skills and experience Solving AI problems from the real problems of the organization Emphasis on knowledge and ability to Training is divided into 5 areas as follows: ",
+        "- Image processing",
+        "- Signal processing",
+        "- Natural language processing/understanding",
+        "- Big Data Analytics",
+        "- Internet of Things and robotics",
+      ],
+    },
+
+    {
+      name: "Google Developer Student Club KMUTT",
+      location: "Bangkok, Thailand", 
+      startDate: "Aug 2021",
+      endDate: "Present",
+      description: [
+        "Developer Student Clubs! As a member, you'll gain access to a global network of students who are passionate about using technology to solve real-world problems. You'll have the opportunity to learn from industry experts, participate in hands-on workshops, and build projects that will help you hone your skills and stand out to potential employers. Don't miss out on this amazing opportunity to kickstart your career in tech!",
+      ],
+    },
+
+    {
+      name: "Working scholarship student, KMUTT",
+      location: "Bangkok, Thailand",
+      startDate: "Sep 2022",
+      endDate: "May 2023",
+      description: [
+        "Working Scholarship is work on Teacher Assistant is subject Css 112 Computer Programming and Make new platform for Mathematic Research Database for Mathematics Department in 2022 -2023",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "King Mongkut's University of Technology Thonburi (KMUTT)", 
+      location: "Bangkok Thailand",
+      startDate: "2021",
+      endDate: "2025",
+      description: [
+        "Computer Science, Faculty of Science", 
+      ],
+    },
+    {
+      name: "Princess Chulabhorn's Science High School Pathum Thani", 
+      location: "Pathum Thani, Thailand", 
+      startDate: "2015",
+      endDate: "2021",
+      description: ["Science , Mathematics and Technology"],
+    }
+  ],
+
+  socialMedia: {
+    facebook: "https://www.facebook.com/tentenTGC/",
+    twitter: "https://twitter.com/tentenTGC",
+    github: "https://github.com/tententgc",
+    email: "thanyapisit.bua@gmail.com",
+    linkedin: "https://www.linkedin.com/in/thanyapisit-buaprakhong-bb0143184/",
+  },
+
+  projects: [
+    {
+      title: "WatchStock",
+      isFeatured: true,
+      thumbnail: "/assets/images/watchstock.png",
+      githubUrl: "https://github.com/tententgc/watchstock-main",
+      liveUrl: "https://watchstock-main.vercel.app/",
+    },
+    {
+      title: "tsp-network",
+      isFeatured: true,
+      thumbnail: "/assets/images/node1.png",
+      githubUrl: "https://github.com/tententgc/tsp-network",
+      liveUrl: "https://github.com/tententgc/tsp-network",
+    },
+    {
+      title: "k8s Todolist",
+      isFeatured: true,
+      thumbnail: "/assets/images/k8s.png",
+      githubUrl: "https://github.com/tententgc/k8s-todolist",
+      liveUrl: "http://20.44.251.80/",
+    },
+    {
+      title: "old : tententgc.com",
+      isFeatured: true,
+      thumbnail: "/assets/images/tententgc-com.png",
+      githubUrl: "https://github.com/tententgc/tententgc.com",
+      liveUrl: "https://oldpage.tententgc.com/",
+    },
+    {
+      title: "tententgc.com",
+      isFeatured: true,
+      thumbnail: "/assets/images/tententgc.png",
+      githubUrl: "https://github.com/tententgc/astro-portfolio",
+      liveUrl: "https://tententgc.com/",
+    },
+  ],
+};
